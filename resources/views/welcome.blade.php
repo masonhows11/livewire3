0a473d875fb24029788128719a4a237aa65fd0a6
mason.hows11 <mason.hows11@gmail.com>
2024-05-16 17:47:53 +0330

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>livewire</title>
+    @livewireStyles
+</head>
+
+<body class="container">
+
+    <div class="row">
+
+
+
+    </div>
+
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    @livewireScripts
+</body>
+
+
+
+</html>
