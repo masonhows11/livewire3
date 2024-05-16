521840760b0de0e05ffb919891e7231e1b06e47f
mason.hows11 <mason.hows11@gmail.com>
2024-05-16 19:13:26 +0330

    user livwires add html element

--- resources/views/admin_end/include/header.blade.php
+++ resources/views/admin_end/include/header.blade.php
@@ -6,7 +6,7 @@
             <!-- begin::breadcrumb -->
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#">داشبورد</a></li>
+                    <li class="breadcrumb-item"><a href="{{  route('admin.panel') }}">داشبورد</a></li>
                     <li class="breadcrumb-item active" aria-current="page"> لیست </li>
                 </ol>
             </nav>
