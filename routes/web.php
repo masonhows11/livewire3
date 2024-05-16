5bb78773120e1fea9fb0ccc023286d27f7acfc51
mason.hows11 <mason.hows11@gmail.com>
2024-05-16 18:11:14 +0330

    add front & admin panel requirement

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\PanelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [HomeController::class,'home'])->name('home');
+
+
+Route::get('/admin/panel', [PanelController::class,'panel'])->name('admin.panel');
+
